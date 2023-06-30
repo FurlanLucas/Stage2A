49939b7a2e536a0ac85a00eb990a66bd74d2ea08
Furlan <lucasrufurlan@gmail.com>
2023-06-30 23:03:05 +0200

    Labview main code v2.0
    Now the acquisition data will be done simultaneously in labview and in DAS240, with a file transfer protocol applied at the end. The conversion between REC file and TXT file is done by seframviewer.

--- exp/labview main project/takeFile.bat
+++ exp/labview main project/takeFile.bat
@@ -3,12 +3,12 @@ rem Il va lire le fichier dedans le DAS240 et faire la convertion par txt
 
 rem --------------------------------------------------------------------
 echo Telechargement du fichier en cours
-start /wait curl -o %1 "http://192.168.137.191/FileSystem/FolderREC/%1"
+start /wait curl -o %2\%1 "http://192.168.137.191/FileSystem/FolderREC/%1"
 echo Saving
 rem --------------------------------------------------------------------
 
 rem --------------------------------------------------------------------
 echo Conversion du fichier en cours
 FOR /F "tokens=2* delims=	 " %%A IN ('REG QUERY "HKCR\recordfiles" /v InstallPath') DO SET Rep=%%B
-"%Rep%\seframviewer.exe"  %1 /t
+"%Rep%\seframviewer.exe" %2\%1 /t
 rem --------------------------------------------------------------------
