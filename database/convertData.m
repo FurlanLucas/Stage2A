81fb5e4dd912c4b55df8a25e18fc6060c595cc07
Furlan <lucasrufurlan@gmail.com>
2023-07-24 13:32:00 +0200

    Database update
    Updates the name of the database.

--- database/convertData.m
+++ database/convertData.m
@@ -12,13 +12,14 @@ function convertData()
     %% Main
     
     dirFileNames = dir(dirInputName + "\*.txt");
-    
+
     for i = 1:length(sysData)  % Cherche chaque système
     
         % Cherche dans le dossier
         found_data = 0; 
-        for j = 1:length(dirFileNames) 
-    
+        Notes = {};
+        for j = 1:length(dirFileNames)
+
             nameDivided = split(dirFileNames(j).name, '_');        
             if strcmp(sysData(i).name, nameDivided{1} + "_" + nameDivided{2})
                 found_data = found_data + 1;
@@ -38,10 +39,13 @@ function convertData()
                 y = sysData(i).setOutput(y - y(1));
     
                 if strcmp(sysData(i).type, 'tension') % asasas
+                    %newDataExp = iddata(sysData(i).toFlux(v), y, Ts);
                     newDataExp = iddata(y, sysData(i).toFlux(v), Ts);
+                    Notes = [Notes, {'NULL'}];
                 else
+                    %newDataExp = iddata(sysData(i).toFlux(phi), y, Ts);
                     newDataExp = iddata(y, sysData(i).toFlux(phi), Ts);
-                    newDataExp.Notes = v;
+                    Notes = [Notes, {v}];
                 end            
                 
                 % Autres informations auxilières
@@ -66,6 +70,7 @@ function convertData()
         
         % Enregistre tous les experiments réalisés
         expData.UserData = sysData(i); 
+        expData.Notes = Notes;
         save(dirOutputName + "\" +sysData(i).name, "expData");
     end
     
