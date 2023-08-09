9906c067a7b897dea3687943d45b05ffb24eb274
Furlan <lucasrufurlan@gmail.com>
2023-08-10 01:19:59 +0200

    Comments update

--- database/toVar.m
+++ database/toVar.m
@@ -1,10 +1,14 @@
-function out = toVar(in)
+function out = toVar(in, samplesNorm)
     %% toVar
     %
-    % Description
+    %   Change la variable d'entrée comme une variation par rapport au
+    %   valeur initiale, en utilisant samplesNorm échantillons. Si aucune
+    %   valeur est passé, la valeur samples Norm = 100 est utilisée.
 
     %% Entrées
-    samplesNorm = 100; % Nombre d'échantillons pour calculler la moyenne ;
+    if nargin == 1
+        samplesNorm = 100; % Nombre d'échantillons pour calculler la moyenne ;
+    end
 
     %% Main
     out = in - mean(in(1:samplesNorm));
