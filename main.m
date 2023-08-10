fe9109123a2f1fd03cc5b9e313b01160cbad9a23
Furlan <lucasrufurlan@gmail.com>
2023-08-10 13:33:53 +0200

    Create main.m
    Create a main file for the code.

---
+++ main.m
@@ -0,0 +1,12 @@
+clc;
+%% Fichier main
+
+fprintf("<strong>ANALYSE PAR LE FICHIER MAIN</strong>\n");
+% Prendre la conversion de variables
+run("database\convertData.m");
+
+% Fait l'analyse fréquencielle
+run("freqAnalysis\main_freqAnalysis.m");
+
+% Fait l'analyse d'identification
+run("identification\main_identification.m");
